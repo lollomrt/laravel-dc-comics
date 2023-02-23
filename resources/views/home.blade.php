f4cd01b4c2dae64fab0fff4000f6807d3ec00dcc
lollomrt <lorenzomartini.mrt@outlook.it>
2023-02-23 14:57:47 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <main class="back-dark">
+        <div class="container-cards position">
+            <span class="label-blue">Current series</span>
+            <div class="row d-flex gap-5 flex-column align-items-center">
+                <div class="col d-flex flex-wrap gap-1-rem w-100">
+                    <h1>questa è la home</h1>
+                </div>
+                <button class="btn btn-primary w-25 text-uppercase">load more</button>
+            </div>
+        </div>
+    </main>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController as ComicController;
+use App\Http\Controllers\PageController as PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [PageController::class,'index'])->name('homepage');
+
+// Route::get('/', function () {
+//     $comics = config('comics');
+//     return view('home', compact('comics'));
+// })->name('homepage');
+
+Route::get('/detail_comic/{slug}', [ComicController::class,'getSingle'])->name('single-comic');
+
+// Route::get('/detail_comic/{id}', function ($id) {
+//     $comics = config('comics');
+//     $single = '';
+//     foreach($comics as $key => $comic){
+//         if($key == $id){
+//             $single = $comic;
+//         }
+//     }
+//     // dd($single);
+//     return view('detail_comic', compact('single'));
+// })->name('single-comic');
